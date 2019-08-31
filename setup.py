77c75c658da9e0187857221779c9b70728f5597d
Jan Dillenberger <jdillenberger@uni-koblenz.de>
2019-08-31 22:07:19 +0200

    Add description to setup

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ with open('README.md', 'r') as README:
         version=os.environ.get('CI_COMMIT_TAG', '0.0.1'),
         author='Jan Dillenberger',
         author_email='jdillenberger@uni-koblenz.de',
+        description='Simple test-suite for json/yaml files, optimized for CI pipelines.',
         long_description=README.read(),
         long_description_content_type='text/markdown',
         url='https://gitlab.rlp.net/jdillenberger/jmatch',
